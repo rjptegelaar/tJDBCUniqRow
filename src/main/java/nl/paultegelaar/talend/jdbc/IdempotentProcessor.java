eeaaeb7b594c5b8a53dff23e224a0570cd62453d
rjptegelaar <tegelaarpaul@gmail.com>
2016-11-08 15:35:28 +0100

    added tests

--- src/main/java/nl/paultegelaar/talend/jdbc/IdempotentProcessor.java
+++ src/main/java/nl/paultegelaar/talend/jdbc/IdempotentProcessor.java
@@ -1,33 +1,29 @@
 package nl.paultegelaar.talend.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.Date;
 
-public class IdempotentProcessor {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
-	private String sqlStatement;
-	private PreparedStatement pStatement;
-	
-	
-	public IdempotentProcessor(String sqlStatement, Connection dbConnection) throws SQLException {
-		super();
-		this.sqlStatement = sqlStatement;
+public class IdempotentProcessor{
 		
-		pStatement = dbConnection.prepareStatement(sqlStatement);
+	private JdbcTemplate jdbcTemplate;
+				
+	public IdempotentProcessor(Connection connection){		
+		jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(connection, false));
+	}	
+	
+	public boolean check(String identifier, Date storeDate, String sqlStatement){
+		try{
+			return jdbcTemplate.update(sqlStatement, new Object[]{identifier,storeDate})==1;	
+		}catch(Exception e){
+			return false;
+		}
 		
 	}
 
-
-
-	public boolean check(String identifier, Date storeDate){
-		
-		
-		
-		return false;
-	}
-
+	
 
 	
 	
---
+++ src/test/java/nl/paultegelaar/talend/jdbc/test/IdempotentProcessorTest.java
@@ -0,0 +1,102 @@
+package nl.paultegelaar.talend.jdbc.test;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import junit.framework.Assert;
+import nl.paultegelaar.talend.jdbc.IdempotentProcessor;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/storage-application-context.xml" })
+public class IdempotentProcessorTest {
+
+	@Autowired
+	private DataSource testDataSource;
+	
+	private IdempotentProcessor idempotentProcessor;
+	
+	
+
+	
+	@Before
+	public void before() throws SQLException{
+		if(idempotentProcessor==null){
+			idempotentProcessor = new IdempotentProcessor(testDataSource.getConnection());
+		}
+		//Just create a table, all in memory anyway
+		Statement st = testDataSource.getConnection().createStatement();
+		st.execute("create table idempotent(id varchar(255) not null unique, created timestamp)");
+		st.close();
+					
+		
+	}
+
+	@After
+	public void end() throws SQLException{
+		//Just drop when done
+		Statement st = testDataSource.getConnection().createStatement();
+		st.execute("drop table idempotent");
+		st.close();		
+	}
+
+	
+	@Test
+	public void happyTest(){
+		
+		Assert.assertTrue(idempotentProcessor.check("test", new Date(), "insert into idempotent values(?, ?)"));
+		Assert.assertTrue(idempotentProcessor.check("test1", new Date(), "insert into idempotent values(?, ?)"));
+		
+	}
+	
+	@Test
+	public void unhappyTest(){
+		
+		Assert.assertTrue(idempotentProcessor.check("test", new Date(), "insert into idempotent values(?, ?)"));
+		Assert.assertFalse(idempotentProcessor.check("test", new Date(), "insert into idempotent values(?, ?)"));
+		
+	}
+	
+	@Test
+	public void testParallel() throws SQLException, InterruptedException{
+		
+		for (int i = 0; i < 5; i++) {
+			TestRunner tr = new TestRunner(idempotentProcessor, String.valueOf(i));
+		}
+		Thread.sleep(10000);
+	}
+	
+	public class TestRunner implements Runnable {
+
+		private String name;
+		
+		private TestRunner(IdempotentProcessor idempotentProcessor, String name) {
+			System.out.println("Spawning thread: " + name);
+			this.name = name;
+			Thread worker = new Thread(this);
+			worker.start();
+		}
+		
+		public void run() {
+			System.out.println("Starting run with thread: " + name);
+			for (int i = 0; i < 100; i++) {
+				Assert.assertTrue(idempotentProcessor.check(name + i, new Date(), "insert into idempotent values(?, ?)"));
+			}
+			System.out.println("Done running thread: " + name);
+		}
+	}
+	
+
+}
